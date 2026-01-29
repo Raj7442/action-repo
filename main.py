022770f966b89a9efccb25fc5f2fd098c3a919e1
Rounak Raj <rounak_2021bmme069@nitsri.ac.in>
2026-01-30 00:17:39 +0530

    Add test function for webhook testing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def hello_world():
     return "Hello from action-repo!"
 
+def test_function():
+    return "Testing webhook integration"
+
 if __name__ == "__main__":
     print(hello_world())
+    print(test_function())
